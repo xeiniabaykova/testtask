564fc0577b4c25b38612e7c2bc476aa4e0df87a4
Ksenia <xeinia.baykova@gmail.com>
2017-05-04 15:06:52 +0300

    fixed tabs

--- Editor/CreatorHandler.h
+++ Editor/CreatorHandler.h
@@ -21,23 +21,25 @@ public:
   //---
   CreatorHandler( int numExpectedPoits, GeometricPrimitiveCreator * creator ):
     numExpectedPoits ( numExpectedPoits ),
-     creator         ( creator )
+    creator          ( creator )
   {
   }
+
 private:
-  CircleCreator( const CircleCreator &obj ) = delete;
-  CircleCreator& operator=( CircleCreator &obj ) = delete;
+  CreatorHandler( const CircleCreator &obj ) = delete;
+  CreatorHandler& operator=( CircleCreator &obj ) = delete;
 
   int                         numExpectedPoits; ///< количество точек, необходимых для создания геометрического примитива
   GeometricPrimitiveCreator * creator;          ///< создатель геометрического примитива
-  std::vector<Point>          points;          ///< точки, полученные с экрана
+  std::vector<Point>          points;           ///< точки, полученные с экрана
+
 public:
- void AddPointFromScreen                       ( Point point ); ///< добавить точку с экрана в массив текущих точек
- void AddSufficientNum                         ( int num );     ///< добавить необходимое для отрисовки кривой количество точек
- bool IsSufficientNum                          () const;        ///< проверить, достаточно ли текущее количество точек для создания и отображения выбранной кривой
- std::shared_ptr<GeometricPrimitive> Create    ();              ///< создать геометрический примитив
- const std::vector<Point>&           RefPoints () const;        ///< вернуть опорные точки
- void ClearPoints                              ();              ///< очистить массив точек, полученных с экрана
+  void                                AddPointFromScreen ( Point point ); ///< добавить точку с экрана в массив текущих точек
+  void                                AddSufficientNum   ( int num );     ///< добавить необходимое для отрисовки кривой количество точек
+  bool                                IsSufficientNum    () const;        ///< проверить, достаточно ли текущее количество точек для создания и отображения выбранной кривой
+  std::shared_ptr<GeometricPrimitive> Create             ();              ///< создать геометрический примитив
+  const std::vector<Point>&           RefPoints          () const;        ///< вернуть опорные точки
+  void                                ClearPoints        ();              ///< очистить массив точек, полученных с экрана
 };
 
 #endif // CREATORHANDLER_H
