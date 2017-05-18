4a551f83c996b6d564837398a3141a5cd1f417ed
Ksenia <xeinia.baykova@gmail.com>
2017-05-18 17:59:50 +0300

    basic nurbs

--- Math/NurbsCurve.cpp
+++ Math/NurbsCurve.cpp
@@ -1,49 +1,142 @@
 #include "NurbsCurve.h"
-#include "Editor/CommonConstants.h"
 
-
-double NurbsCurve::DistancePointToCurve( Point point ) const
+double NurbsCurve::DistancePointToCurve(Point point) const
 {
+  double accuracy = 0.01;
   std::vector<Point> polylinePoints;
-  GetAsPolyLine( polylinePoints, CommonConstants::PRECISION_POLYLINE );
- // return C2Curve::DistancePointToCurve( point, polylinePoints );
+  GetAsPolyLine(polylinePoints, accuracy);
+  // return C2Curve::DistancePointToCurve( point, polylinePoints );
   return 0.0;
 }
 
-
-void NurbsCurve::ComputeBasicFunctions( double x,int i, std::vector<std::vector<double>>& basicFunctions )
+void NurbsCurve::ComputeBasicFunction(double x, int i,double& result) const
 {
-  basicFunctions.resize( deg );
-  for ( int k = 0; k < deg; k++ )
-    basicFunctions.resize( deg );
-
-  basicFunctions[0][0] = 1.0;
-  std::vector<double> Left;
-  Left.resize( deg + 1 );
-  std::vector<double> Right;
-  Right.resize( deg + 1 );
-
-  for ( int j = 1; j <= deg; j++ )
+  i = 3;
+  double p = degree;
+  std::vector<double> N;
+  N.resize( p + 1);
+  if ((i == 0 && x == nodes[0]) ||
+    (i == nodes.size() - p - 2 && x == nodes[nodes.size()]))
   {
-    Left[j] = x - nodes[i + 1 -j];
-    Right[j] = nodes[i + j] - x;
-    double savedValue = 0.0;
-    for ( int k = 0; k < j; k++ )
-    {
-    basicFunctions[j][k] = Right[j - 1] + Left[j -k];
-    double temp = basicFunctions[k][j - 1] / basicFunctions[k][j];
-    basicFunctions[k][j] = savedValue + Right[k+1] * temp;
-    savedValue = Left[j - k] * temp;
-    }
-    basicFunctions[j][j] = savedValue;
+    result = 1.0;
+    return;
   }
+  if (x < nodes[i] || x >= nodes[i + p + 1])
+  {
+    result = 0.0;
+    return;
+  }
+  for (int j = 0; j <= p; j++)
+  {
+    if (x >= nodes[i + j] && x < nodes[i + j + 1])
+      N[j] = 1.0;
+    else
+      N[j] = 0.0;
+  }
+  for (int k = 1; k <= p; k++)
+  {
+    double saved;
+    if (N[0] == 0.0)
+      saved = 0.0;
+    else
+      saved = ((x - nodes[i]) * N[0]) / (nodes[i + k] - nodes[i]);
+    for (int j = 0; j < p - k + 1; j++)
+    {
+      double uLeft = nodes[i + j + 1];
+      double uRight = nodes[i + j + k + 1];
+      if (N[j + 1] == 0.0)
+      {
+        N[j] = saved;
+        saved = 0.0;
+      }
+      else
+      {
+        double temp = N[j + 1] / (uRight - uLeft);
+        N[j] = saved + (uRight - x)* temp;
+        saved = (x - uLeft) * temp;
+      }
+    }
+  }
+  result = N[0];
 }
 
-void NurbsCurve::ComputeBasicFunctionsD( double X, const std::vector<std::vector<double>>& basicFunctions, std::vector<std::vector<double>>& basicFunctionsD )
+double NurbsCurve::FindSpan(double x) const
 {
-  for ( int k=0; k<= deg; k++ )
+  if (x == nodes[nodes.size()]) return nodes.size();
+  double low = degree;
+  double hight = nodes.size();
+  double mid = (low + hight) / 2.0;
+  while ((x < nodes[mid + 1]) || (x >= nodes[mid + 1]))
   {
-    double s1 =0;
-    double s2 =0;
+    if (x < nodes[mid])
+      hight = mid;
+    else
+      low = mid;
+    mid = (low + hight) / 2.0;
   }
+  return mid;
+}
+
+void NurbsCurve::ComputeBasicFunctionsD(double X, const std::vector<std::vector<double>>& basicFunctions, std::vector<std::vector<double>>& basicFunctionsD)
+{
+
+}
+double NurbsCurveBasicFunction()
+{
+  return 0.0;
+}
+
+void NurbsCurve::Translation(double xShift, double yShift)
+{
+
+}
+void NurbsCurve::Rotation(double alpha)
+{
+
+}
+void NurbsCurve::Dilatation(double XScaling, double YScaling)
+{
+
+}
+
+double NurbsCurve::CountWeight(double x)  const
+{
+  double w = 0.0;
+  for (int i = 0; i < weights.size(); i++)
+  {
+    double result;
+    ComputeBasicFunction(x, i, result);
+    w += result*weights[i];
+  }
+  return w;
+}
+Point  NurbsCurve::GetPoint(double t) const
+{
+  double span = FindSpan(t);
+  double result;
+  double weightNurbs = CountWeight(t);
+  Point resultPoint;
+  for (int i = 0; i < poles.size(); i++)
+  {
+    double result;
+    ComputeBasicFunction(t, span, result);
+    resultPoint = resultPoint + poles[i] * result*weights[i];
+  }
+  return resultPoint;
+}
+Point  NurbsCurve::GetDerivativePoint(double t) const
+{
+  return Point(0.0, 0.0);
+}
+Point  NurbsCurve::Get2DerivativePoint(double t) const
+{
+  return Point(0.0, 0.0);
+}
+Range  NurbsCurve::GetRange() const
+{
+  return Range(0.0, 0.0);
+}
+void  NurbsCurve::GetAsPolyLine(std::vector<Point> & polyLinePoints, double accuracy) const
+{
+
 }
--- Math/NurbsCurve.h
+++ Math/NurbsCurve.h
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 /**
-  \file
-  \brief \ru  nurbs - кривая в двумерном пространстве\~
+\file
+\brief \ru  nurbs - кривая в двумерном пространстве\~
 
 */
 ////////////////////////////////////////////////////////////////////////////////
@@ -10,8 +10,8 @@
 #ifndef NURBSCURVE_H
 #define NURBSCURVE_H
 #include "C2Curve.h"
-#include <Math/Point.h>
-#include <Math/Range.h>
+#include "Point.h"
+#include "Range.h"
 #include <vector>
 #include "GeometricPrimitive.h"
 
@@ -22,45 +22,53 @@
 // ---
 
 
-class NurbsCurve: public GeometricPrimitive
+class NurbsCurve : public GeometricPrimitive
 {
 private:
-  int deg;
   std::vector<Point>  poles;
   std::vector<double> weights;
-  std::vector<int>    nodes;
+  std::vector<double> nodes;
   bool                isClosed;
-  bool isValid;
+  double              degree;
+
 private:
-  NurbsCurve( const NurbsCurve &obj ) = delete;
-  NurbsCurve& operator=( NurbsCurve &obj ) = delete;
+  NurbsCurve(const NurbsCurve &obj) = delete;
+  NurbsCurve& operator=(NurbsCurve &obj) = delete;
 public:
   NurbsCurve() = delete;
   virtual ~NurbsCurve() = default;
   /**  \brief \ru создать nurbs - кривую по опорным точкам, весам, опорным точкам и замкнутости
-    \param[in] points - опорные точки .\~
-    \param[in] referencePoints - \ru опорные точки .\~
-    \param[in] weights - \ru веса .\~
-     \param[in] isClosed - \ru замкнутость .\~
+  \param[in] points - опорные точки .\~
+  \param[in] referencePoints - \ru опорные точки .\~
+  \param[in] weights - \ru веса .\~
+  \param[in] isClosed - \ru замкнутость .\~
   */
   //---
-  NurbsCurve ( const std::vector<Point>& poles, const std::vector<double>& weights,
-               const std::vector<int>& nodes, bool isClosed );
-  virtual Point  GetPoint            ( double t ) const;                                             ///< возвращает точку на кривой по параметру t
-  virtual Point GetDerivativePoint   ( double t ) const;                                             ///< возвращает производную по параметру t
-  virtual Point Get2DerivativePoint  ( double t ) const;                                             ///< возвращает вторую производную по параметру t
-  virtual Range GetRange             () const;                                                       ///< возвращает вторую производную по параметру t
-  virtual void  GetAsPolyLine        ( std::vector<Point> & polyLinePoints, double accuracy ) const; ///< возвращает полилинию для Nurbs - кривой с точностью accuracy
-         double DistancePointToCurve ( Point point ) const;
-  virtual void  Translation          ( const std::vector<std::vector<double>>& transformationMatrix );
-  virtual void  Rotation             ( const std::vector<std::vector<double>>& transformationMatrix );
-  virtual void  Dilatation           ( const std::vector<std::vector<double>>& transformationMatrix );
-  bool          IsValid();
-private:
-  void ComputeBasicFunctions(  double x,int i, std::vector<std::vector<double>>& basicFunctions );
-  void ComputeBasicFunctionsD( double X, const std::vector<std::vector<double>>& basicFunctions, std::vector<std::vector<double>>& basicFunctionsD );
-  double BasicFunction();
+  NurbsCurve( const std::vector<Point>& poles, const std::vector<double>& weights,
+    const std::vector<double>& nodes, bool isClosed, int degree ):
+    poles(poles),
+    weights(weights),
+    nodes(nodes),
+    isClosed(isClosed),
+    degree(degree)
+  {
+  }
 
+  virtual Point  GetPoint               ( double t ) const;                                             ///< возвращает точку на кривой по параметру t
+  virtual Point  GetDerivativePoint     ( double t ) const;                                             ///< возвращает производную по параметру t
+  virtual Point  Get2DerivativePoint    ( double t ) const;                                             ///< возвращает вторую производную по параметру t
+  virtual Range  GetRange               ()         const;                                                       ///< возвращает вторую производную по параметру t
+  virtual void   GetAsPolyLine          ( std::vector<Point> & polyLinePoints, double accuracy ) const; ///< возвращает полилинию для Nurbs - кривой с точностью accuracy
+  double         DistancePointToCurve   ( Point point ) const;
+  virtual void   Translation            ( double xShift, double yShift );
+  virtual void   Rotation               ( double alpha );
+  virtual void   Dilatation             ( double XScaling, double YScaling );
+
+  double         CountWeight            ( double x ) const;
+  double         FindSpan               ( double x ) const;
+  void           ComputeBasicFunction   ( double x, int i, double & result ) const;
+  void           ComputeBasicFunctionsD ( double X, const std::vector<std::vector<double>>& basicFunctions, std::vector<std::vector<double>>& basicFunctionsD );
+  double         BasicFunction          ();
 };
 
 
