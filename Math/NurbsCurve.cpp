05084e495df11a526511600649f191469c9f0b85
Ksenia <xeinia.baykova@gmail.com>
2017-05-19 11:43:30 +0300

    fixed Nurbs curve derivatives

--- Math/NurbsCurve.cpp
+++ Math/NurbsCurve.cpp
@@ -9,58 +9,58 @@ double NurbsCurve::DistancePointToCurve(Point point) const
   return 0.0;
 }
 
-void NurbsCurve::ComputeBasicFunction(double x, int i,double& result) const
+void NurbsCurve::ComputeBasicFunction( double x, int i,double& result ) const
 {
-  i = 3;
   double p = degree;
   std::vector<double> N;
   N.resize( p + 1);
-  if ((i == 0 && x == nodes[0]) ||
-    (i == nodes.size() - p - 2 && x == nodes[nodes.size()]))
+
+  if ( (i == 0 && x == nodes[0]) ||
+    (i == nodes.size() - p - 2 && x == nodes[nodes.size()]) )
   {
     result = 1.0;
     return;
   }
-  if (x < nodes[i] || x >= nodes[i + p + 1])
+  if (x < nodes[i] || x >= nodes[i + p + 1] )
   {
     result = 0.0;
     return;
   }
-  for (int j = 0; j <= p; j++)
+  for ( int j = 0; j <= p; j++ )
   {
-    if (x >= nodes[i + j] && x < nodes[i + j + 1])
+    if ( x >= nodes[i + j] && x < nodes[i + j + 1] )
       N[j] = 1.0;
     else
       N[j] = 0.0;
   }
-  for (int k = 1; k <= p; k++)
+  for ( int k = 1; k <= p; k++ )
   {
-    double saved;
-    if (N[0] == 0.0)
+    double saved = 0.0;
+    if ( N[0] == 0.0 )
       saved = 0.0;
     else
-      saved = ((x - nodes[i]) * N[0]) / (nodes[i + k] - nodes[i]);
-    for (int j = 0; j < p - k + 1; j++)
+      saved = ( (x - nodes[i]) * N[0]) / (nodes[i + k] - nodes[i]);
+    for ( int j = 0; j < p - k + 1; j++ )
     {
       double uLeft = nodes[i + j + 1];
       double uRight = nodes[i + j + k + 1];
-      if (N[j + 1] == 0.0)
+      if ( N[j + 1] == 0.0 )
       {
         N[j] = saved;
         saved = 0.0;
       }
       else
       {
-        double temp = N[j + 1] / (uRight - uLeft);
-        N[j] = saved + (uRight - x)* temp;
-        saved = (x - uLeft) * temp;
+        double temp = N[j + 1] / ( uRight - uLeft );
+        N[j] = saved + ( uRight - x )* temp;
+        saved = ( x - uLeft ) * temp;
       }
     }
   }
   result = N[0];
 }
 
-double NurbsCurve::FindSpan(double x) const
+double NurbsCurve::FindSpan( double x ) const
 {
   if (x == nodes[nodes.size()]) return nodes.size();
   double low = degree;
@@ -77,8 +77,78 @@ double NurbsCurve::FindSpan(double x) const
   return mid;
 }
 
-void NurbsCurve::ComputeBasicFunctionsD(double X, const std::vector<std::vector<double>>& basicFunctions, std::vector<std::vector<double>>& basicFunctionsD)
+void NurbsCurve::ComputeBasicFunctionD( double x, int i, double& result, int derivativeOrder ) const
 {
+  if ( x < nodes[i] || x >= nodes[i + degree + 1] )
+  {
+    result = 0.0;
+    return;
+  }
+  std::vector<std::vector<double>> vNodes;
+  vNodes.resize( degree + 1 );
+  for ( int i = 0; i < degree + 1; i++ )
+    vNodes[degree + 1].resize( degree + 1 );
+
+  for ( int j = 0; j <= degree; j++ )
+  {
+    if ( (x >= nodes[i + j]) && (x < nodes[i + j + 1]) )
+      vNodes[j][0] = 1.0;
+    else
+      vNodes[j][0] = 0.0;
+  }
+  double saved;
+  for (int k = 1; k <= degree; k++)
+  {
+    if ( vNodes[0][k - 1] == 0.0 )  saved = 0.0;
+    else saved = ( (x - nodes[i]) * vNodes[0][k - 1] ) / ( nodes[i + k] - nodes[i] );
+    for ( int j = 0; j < degree - k + 1; j++ )
+    {
+      double uLeft = nodes[i + j + 1];
+      double uRight = nodes[i + j + k + 1];
+      if ( vNodes[j + 1][k - 1] == 0.0 )
+      {
+        vNodes[j][k] = saved;
+        saved = 0.0;
+      }
+      else
+      {
+        double temp = vNodes[j + 1][k - 1] / ( uRight - uLeft );
+        vNodes[j][k] = saved + ( uRight - x ) * temp;
+        saved = ( x - uLeft ) * temp;
+      }
+    }
+  }
+  std::vector<double> derivative;
+  for ( int k = 1; k <= derivativeOrder; k++ )
+  {
+    for ( int j = 0; j <= x; j++ )
+      derivative[j] = vNodes[j][degree - k];
+
+    double saved = 0.0;
+    for ( int m = 1; m <= k; m++ )
+    {
+      if ( derivative[0] == 0.0 ) saved = 0.0;
+      else
+        saved = derivative[0] / ( nodes[i + p - k + m] / nodes[i] );
+      for ( int j = 0; j < k - m + 1; j++ )
+      {
+        double uLeft = nodes[i + j + 1];
+        double uRight = nodes[i + j + 1];
+        if ( derivative[j + 1] == 0.0 )
+        {
+          derivative[j] = saved;
+          saved = 0.0;
+        }
+        else
+        {
+          double temp = derivative[j + 1] / ( uRight - uLeft );
+          derivative[j] = saved + ( uRight + x ) *temp;
+          saved = ( x - uLeft ) * temp;
+        }
+      }
+    }
+    result = derivative[0];
+  }
 
 }
 double NurbsCurveBasicFunction()
@@ -86,57 +156,99 @@ double NurbsCurveBasicFunction()
   return 0.0;
 }
 
-void NurbsCurve::Translation(double xShift, double yShift)
+void NurbsCurve::Translation( double xShift, double yShift )
 {
 
 }
-void NurbsCurve::Rotation(double alpha)
+void NurbsCurve::Rotation( double alpha )
 {
 
 }
-void NurbsCurve::Dilatation(double XScaling, double YScaling)
+void NurbsCurve::Dilatation( double XScaling, double YScaling )
 {
 
 }
 
-double NurbsCurve::CountWeight(double x)  const
+double NurbsCurve::CountWeight( double x )  const
 {
   double w = 0.0;
-  for (int i = 0; i < weights.size(); i++)
+  for ( int i = 0; i < weights.size(); i++ )
   {
     double result;
-    ComputeBasicFunction(x, i, result);
+    ComputeBasicFunction( x, i, result );
+    w += result * weights[i];
+  }
+  return w;
+}
+
+double NurbsCurve::CountWeightD( double t )  const
+{
+  double w = 0.0;
+  for ( int i = 0; i < weights.size(); i++ )
+  {
+    double result;
+    ComputeBasicFunctionD( t, i, result, 1 );
     w += result*weights[i];
   }
   return w;
 }
-Point  NurbsCurve::GetPoint(double t) const
+
+double NurbsCurve::CountWeightD2( double t )  const
 {
-  double span = FindSpan(t);
-  double result;
-  double weightNurbs = CountWeight(t);
+  double w = 0.0;
+  for ( int i = 0; i < weights.size(); i++ )
+  {
+    double result;
+    ComputeBasicFunctionD( t, i, result, 2 );
+    w += result * weights[i];
+  }
+  return w;
+}
+Point NurbsCurve::GetPoint( double t ) const
+{
+  double span = FindSpan( t );
+  double weightNurbs = CountWeight( t );
+  Point resultPoint;
+  for ( int i = 0; i < poles.size(); i++ )
+  {
+    double result;
+    ComputeBasicFunction( t, span, result );
+    resultPoint = resultPoint + poles[i] * result * weights[i];
+  }
+  return resultPoint / weightNurbs;
+}
+Point  NurbsCurve::GetDerivativePoint( double t ) const
+{
+  double span = FindSpan( t );
+  double weightNurbsD = CountWeightD( t );
+  Point resultPoint;
+  for ( int i = 0; i < poles.size(); i++ )
+  {
+    double result;
+    ComputeBasicFunctionD( t, span, result, 1);
+    resultPoint = resultPoint + poles[i] * result * weights[i];
+  }
+  return resultPoint /weightNurbsD ;
+
+}
+Point  NurbsCurve::Get2DerivativePoint( double t ) const
+{
+  double span = FindSpan( t );
+  double weightNurbsD2 = CountWeightD2( t );
   Point resultPoint;
   for (int i = 0; i < poles.size(); i++)
   {
     double result;
-    ComputeBasicFunction(t, span, result);
-    resultPoint = resultPoint + poles[i] * result*weights[i];
+    ComputeBasicFunctionD( t, span, result, 2 );
+    resultPoint = resultPoint + poles[i] * result * weights[i];
   }
-  return resultPoint;
-}
-Point  NurbsCurve::GetDerivativePoint(double t) const
-{
-  return Point(0.0, 0.0);
-}
-Point  NurbsCurve::Get2DerivativePoint(double t) const
-{
-  return Point(0.0, 0.0);
+  return resultPoint / weightNurbsD2;
 }
 Range  NurbsCurve::GetRange() const
 {
   return Range(0.0, 0.0);
 }
-void  NurbsCurve::GetAsPolyLine(std::vector<Point> & polyLinePoints, double accuracy) const
+void  NurbsCurve::GetAsPolyLine( std::vector<Point> & polyLinePoints, double accuracy ) const
 {
 
 }
