4a35088c231e87ea8a589f0a3dd67869dc1e2c29
Ksenia <xeinia.baykova@gmail.com>
2017-06-06 09:49:23 +0300

    fixed nurbs

--- Math/Vector.cpp
+++ Math/Vector.cpp
@@ -1,27 +1,58 @@
 #include "Vector.h"
 
 namespace Math {
-Vector::Vector(double x1, double x2):
-  x(x1),
-  y(x2)
+
+//----------------------------------------------------------------------------
+/**
+	Коструткор по двум значениям.
+*/
+//---
+Vector::Vector( double x1, double x2 ):
+  x( x1 ),
+  y( x2 )
 {
 }
-Vector::Vector(const Vector& vector)
+
+
+//----------------------------------------------------------------------------
+/**
+	Коструткор копии.
+*/
+//---
+Vector::Vector( const Vector& vector )
 {
   x = vector.x;
   y = vector.y;
 }
 
+
+//----------------------------------------------------------------------------
+/**
+	Получить координату x.
+*/
+//---
 double Vector::GetX() const
 {
   return x;
 }
 
+
+//----------------------------------------------------------------------------
+/**
+	Получить координату y.
+*/
+//---
 double Vector::GetY() const
 {
   return y;
 }
 
+
+//----------------------------------------------------------------------------
+/**
+	Сдвинуть по осям x, y на xShift, yShift .
+*/
+//---
 void Vector::Translate ( double xShift, double yShift )
 {
   x = x + xShift;
@@ -29,6 +60,12 @@ void Vector::Translate ( double xShift, double yShift )
 
 }
 
+
+//----------------------------------------------------------------------------
+/**
+	Повернуть на угол alpha относительно начала координат.
+*/
+//---
 void Vector::Rotate( double alpha )
 {
   double cosAlpha = cos( alpha );
@@ -39,6 +76,12 @@ void Vector::Rotate( double alpha )
   y = oldx * sinAlpha + oldy * cosAlpha;
 }
 
+
+//----------------------------------------------------------------------------
+/**
+	Маштабировать по оси х на xScaling, по оси y на yScaling.
+*/
+//---
 void Vector::Scale( double xScaling, double yScaling )
 {
   x *= xScaling;
@@ -47,18 +90,44 @@ void Vector::Scale( double xScaling, double yScaling )
 }
 
 
+//----------------------------------------------------------------------------
+/**
+	Домножить вектор на скаляр.
+*/
+//---
 Vector Vector::operator * ( double param  ) const
 {
   return Vector( x * param, y * param );
 }
+
+
+//----------------------------------------------------------------------------
+/**
+	Получить разность векторов.
+*/
+//---
 Vector Vector::operator - ( Vector point ) const
 {
   return Vector( x - point.GetX(), y -point.GetY() );
 }
+
+
+//----------------------------------------------------------------------------
+/**
+	Получить сумму векторов.
+*/
+//---
 Vector Vector::operator + ( Vector point ) const
 {
   return Vector( x + point.GetX(), y + point.GetY() );
 }
+
+
+//----------------------------------------------------------------------------
+/**
+	Присвоить один вектор другому.
+*/
+//---
 Vector Vector::operator = ( Vector point )
 {
   x = point.GetX();
@@ -66,10 +135,23 @@ Vector Vector::operator = ( Vector point )
   return *this;
 }
 
+
+//----------------------------------------------------------------------------
+/**
+	Получить скалярное произведение векторов по определению.
+*/
+//---
 double Vector::operator * ( Vector vector ) const
 {
   return x * vector.x + y * vector.y;
 }
+
+
+//----------------------------------------------------------------------------
+/**
+	Проверить вектор на правильность.
+*/
+//---
 bool Vector::IsValid() const
 {
   return true;
